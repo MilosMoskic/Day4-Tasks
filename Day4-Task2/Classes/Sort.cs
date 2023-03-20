823072283e2165e95ffe5dbeb85c3586e392bb97
m.moskic-int <m.moskic-int@INTERNSHIP-ZR-2>
2023-03-20 12:47:36 +0100

    Removen unnecessary usings

--- Day4-Task2/Classes/Sort.cs
+++ Day4-Task2/Classes/Sort.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day4_Task2.Classes
+﻿namespace Day4_Task2.Classes
 {
     public abstract class Sort
     {
